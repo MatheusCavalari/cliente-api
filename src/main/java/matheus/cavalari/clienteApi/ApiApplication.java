6ba2547daf306e90f120f628309b180462ea7015
Matheus Cavalari <matheuscavbarbosa@hotmail.com>
2024-05-31 10:55:14 -0300

    Initial commit

---
+++ src/main/java/matheus/cavalari/clienteApi/ApiApplication.java
@@ -0,0 +1,15 @@
+package matheus.cavalari.clienteApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"matheus.cavalari.clienteApi"})
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
